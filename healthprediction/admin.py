d73df69fde3544b40012944d00264b8fb2eb1cf5
NidhiDangi08 <nidhidangi62@gmail.com>
2024-11-27 12:12:32 +0530

    Delete healthprediction directory

--- healthprediction/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from healthprediction.models import Symtoms
-from .models import DiabetesPredict
-
-admin.site.register(Symtoms)
-admin.site.register(DiabetesPredict)
--- healthprediction/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HealthpredictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'healthprediction'
